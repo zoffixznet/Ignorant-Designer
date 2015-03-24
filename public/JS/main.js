43a550d770f19516d2431d7d4ff919688bbb4a71
Zoffix Znet <cpan@zoffix.com>
2015-03-23 20:01:50 -0400

    Made index styles spiffier

---
+++ public/JS/main.js
@@ -0,0 +1,18 @@
+jQuery(function ($) {
+    if ( $('#home_pic') ) {
+        $(window).scroll(function() {
+            if( $(window).scrollTop() > 20 ) {
+                $('#home_pic').animate(
+                    {
+                        'top': -820,
+                        'margin-top': -520
+                    },
+                    700,
+                    function(){
+                        $('#home_pic').css('display', 'none');
+                    }
+                );
+            }
+        });
+    }
+});
